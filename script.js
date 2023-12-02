49bf4e4fd442075787ff7922af5a2cdc73faa8cd
Peteysanz <152723910+Peteysanz@users.noreply.github.com>
2023-12-02 20:00:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,9 +12,18 @@ function updateImage() {
 
     if (diffDays <= 22) {
         const imageNumber = diffDays + 1;
-        const imagePath = `images/image${imageNumber}.jpg`; // Update path
+        const imagePath = `images/image${imageNumber}.jpg`;
+        const imageLinks = [
+            'https://www.youtube.com/watch?v=y6120QOlsfU',  // Link for image 1
+            'https://link2.com',  // Link for image 2
+            // Add links for all images, use '#' or '' for no link
+        ];
+
+        const imageLink = imageLinks[imageNumber - 1] || '#'; // Default to '#' if no link is provided
         document.getElementById('dailyImage').src = imagePath;
+        document.getElementById('imageLink').href = imageLink;
     }
 }
+
 
 document.addEventListener('DOMContentLoaded', updateImage);
